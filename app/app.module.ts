282002017b2cd32cd9956f78d44ec4154fe982d7
Razi Ahmad <razizhcet@gmail.com>
2018-04-30 19:01:26 +0530

    amgular material

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,25 +1,23 @@
-import { CoursesService } from './courses.service';
-import { CoursesComponent } from './courses.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 
 import { AppComponent } from './app.component';
-import { CourseComponent } from './course/course.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CoursesComponent,
-    CourseComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCheckboxModule
   ],
-  providers: [
-    CoursesService
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
